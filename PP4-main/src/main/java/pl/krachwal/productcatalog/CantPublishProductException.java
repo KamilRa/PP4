98f335e32ce4098d42aa793605b81d99f46947b0
Kamil Rachwał <77104064+KamilRa@users.noreply.github.com>
2022-04-04 12:55:36 +0200

    Add files via upload

---
+++ PP4-main/src/main/java/pl/krachwal/productcatalog/CantPublishProductException.java
@@ -0,0 +1,5 @@
+package pl.krachwal.productcatalog;
+
+public class CantPublishProductException
+    extends IllegalStateException {
+}
---
+++ PP4-main/src/test/java/pl/krachwal/productcatalog/SqlProductCatalogeTest.java
@@ -0,0 +1,26 @@
+package pl.krachwal.productcatalog;
+
+import org.junit.jupiter.api.Test;
+
+public class SqlProductCatalogeTest {
+    
+    @Test
+    void itAllowsToStoreAndLoadProduct() {
+    ProductData product = thereIsExampleProduct();
+    ProductStorage listProductStorage = thereIsListProductStorage();
+    
+    listProductStorage.save(product);
+    ProductData loaded = listProductStorage.load(product.getId());
+
+
+        assertsEquals(product.getId(), loaded.getId());
+    assertsEquals(product.getName(), loaded.getName());
+    }
+
+    private ProductStorage thereIsListProductStorage() {
+    }
+
+    private void thereIsExampleProduct() {
+    }
+}
+}
